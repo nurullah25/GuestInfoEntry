a2157c5f45d2a2a6581296a6a06b8cbaba453f34
Nur Mohammad Nurullah <nur447544@gmail.com>
2023-02-09 15:37:10 +0600

    first commit

---
+++ GuestInfoForm/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GuestInfoForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> gender = new List<string>()
+            {
+                "Male",
+                "Female",
+                "Others"
+            };
+            this.cmbGender.ItemsSource = gender;
+            cmbGender.Text = "";
+            List<string> roomType = new List<string>()
+            {
+                "Economy",
+                "Business Class",
+                "Suite"
+            };
+            this.cmbRoomType.ItemsSource = roomType;
+            cmbRoomType.Text = "Economy";
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ GuestInfoForm/Models/Guests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestInfoForm.Models
+{
+    public class Guests
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public string RoomType { get; set; }
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+
+
+
+    }
+}
